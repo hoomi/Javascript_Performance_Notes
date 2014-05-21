fb5ddc5ab247503910b533ffe1f6b6f603396061
hoomi <hoomi2010@gmail.com>
2014-05-21 08:12:12 +0100

    Adds if condition comparison

---
+++ javascripts/testifcondition.js
@@ -0,0 +1,35 @@
+var findMultiplesFast = function findMultiplesFast() {
+  var foundValues=[0];
+  for (var i = 1; i<=multipleValue ; i++) {
+    if (i%3498 == 0 && 
+    	i%1290 ==0 &&
+    	i%167 == 0 &&
+    	i% 53 == 0 &&
+    	i%10 == 0 &&
+    	i%2 == 0 &&
+    	foundValues[0] == 0) {
+    	foundValues.push(i);
+    }
+  }
+}
+
+var findMultiplesSlow = function findMultiplesSlow() {
+  var foundValues=[];
+  for (var i = 0; i<=multipleValue ; i++) {
+    if (foundValues[0] == 0 &&
+    	i%2 == 0 && 
+    	i%10 ==0 &&
+    	i%53 == 0 &&
+    	i%167 == 0 &&
+    	i%1290 == 0 &&
+    	i%3498 == 0) {
+    	foundValues.push(i);
+    }
+  }
+}
+
+var compareFindMultiples = function compareFindMultiples()
+{
+  var a = [findMultiplesSlow,findMultiplesFast];
+  compareFunctions(a);
+}
